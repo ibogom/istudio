da7ab098aa1baeda9fff5999f8bd8b5307525033
snoppy911 <snoppy911@gmail.com>
2014-03-08 01:08:18 +0200

    content and header
    completed header and content

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="ie6 oldie"> <![endif]-->
+<!--[if IE 7]>    <html class="ie7 oldie"> <![endif]-->
+<!--[if IE 8]>    <html class="ie8 oldie"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Документ без названия</title>
+<link href="css/boilerplate.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<!--[if lt IE 9]>
+<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<script src="js/respond.min.js"></script>
+</head>
+<body>
+  <div class="header">
+  	<div class="main">
+    	<div class="menu">
+        	<div><a href="#">О Нас</a></div>
+            <div><a href="#">Цены</a></div>
+            <div><a href="#">Портфолио</a></div>
+            <div><a href="#">Контакты</a></div>
+        </div>
+    </div>
+  </div>
+  <div class="main">
+      <div class="logo_wrapper">
+        <div class="logo">
+            <img src="img/logo.png" />
+            <p><span>мир</span> ваших возможностей</p>
+        </div>
+      </div>
+  </div>
+  <div class="content">
+      	<h2>НАШИ УСЛУГИ</h2>
+        <div class="left_content">
+        <img class="design" src="img/design.png">
+        <h4>ДИЗАЙН</h4>
+        <p>Наша вебстудия не занимаеться копирование чужих макетов или идей.Поэтому наши заказчики могут быть уверены в получении абсолютно индивидуального дизайна заточенного конкретно под них.</p>
+        </div>
+        <div class="center_content">
+        <img class="construct" src="img/construct.png">
+        <h4>РАЗРАБОТКА</h4>
+        <p>Вы хотите чтобы на вашем сайте можно было голосовать? Хотите видеть красивую анимацию фотографий? Вам необходимо оставлять комментарии под вашими статьями? На все эти и многие другие капризы ответить наша комманда разработчиков, которая сделает ваш сайт таким, каким вы его хотели бы видеть!</p>
+        </div>
+        <div class="right_content">
+        <img class="seo" src="img/seo.png">
+        <h4>SEO</h4>
+        <p>У Вас есть сайт, но на него никто не заходит? Не беда,наша команда профессионалов по оптимизации сайта, поможет продвинуть ваш сайт в кротчайшие сроки, по очень демократическим ценам.</p>
+        </div>    
+  </div>
+</body>
+</html>
